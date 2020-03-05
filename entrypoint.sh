361bf6072b2b2c281b6d7d4924cd14f7428ea40d
Darío Blanco <dblancoit@gmail.com>
2020-03-05 18:04:17 +0100

    Add prefix for release bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,9 +41,9 @@ fi
 # get commit logs and determine home to bump the version
 # supports #major, #minor, #patch (anything else will be 'minor')
 case "$LOG" in
-    *#major* ) VERSION=$(semver bump major "$VERSION");;
-    *#minor* ) VERSION=$(semver bump minor "$VERSION");;
-    *#patch* ) VERSION=$(semver bump patch "$VERSION");;
+    *#${PREFIX}major* ) VERSION=$(semver bump major "$VERSION");;
+    *#${PREFIX}minor* ) VERSION=$(semver bump minor "$VERSION");;
+    *#${PREFIX}patch* ) VERSION=$(semver bump patch "$VERSION");;
     * ) VERSION=$(semver bump "$DEFAULT_BUMP" "$VERSION");;
 esac
 
